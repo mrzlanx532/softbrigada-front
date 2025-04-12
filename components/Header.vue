11930d8c4b9b16dfe62626ebe8493fe3536363ad
Denis Danilov <d.danilov@duotek.ru>
2025-04-12 16:57:08 +0300

    Init

---
+++ components/Header.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import Button from '~/components/Button.vue'
+</script>
+
+<template>
+  <div class="header">
+    <div class="header__container">
+      <div class="header__logo">logo</div>
+      <div class="header__menu">
+        <a>что мы делаем</a>
+        <a>наши кейсы <span>+3</span></a>
+      </div>
+    </div>
+    <div class="header__container --right">
+      <div class="header__contacts">
+        <div class="header__contacts-social">
+          <svg><use href="/sprite.svg#telegram" /></svg>
+          <svg><use href="/sprite.svg#whatsapp" /></svg>
+        </div>
+        <a>info@gmail.com</a>
+        <a>+7 (910) 292-03-10</a>
+      </div>
+      <Button class="header__button">Заполнить бриф</Button>
+    </div>
+  </div>
+</template>
---
+++ components/sections/Hero.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import Header from '~/components/Header.vue'
+import Button from '~/components/Button.vue'
+</script>
+
+<template>
+  <Header />
+  <section id="hero" class="hero">
+    <h1>
+      <span>Разрабатываем</span>
+      <span>IT-решения для бизнеса</span>
+      <span>— от идеи до запуска</span>
+    </h1>
+    <div class="hero__buttons">
+      <Button class="--white">Получить решение</Button>
+      <Button>Смотреть кейсы</Button>
+    </div>
+  </section>
+</template>
