53214af5dd27266ad4b5b5a2d18bc0bb270091eb
Denis Danilov <d.danilov@duotek.ru>
2025-04-14 17:05:58 +0300

    Temp

---
+++ components/sections/Offer.vue
@@ -0,0 +1,85 @@
+<template>
+  <section id="offer" class="offer">
+    <div class="offer__wrapper">
+      <h2>Как предприниматель рассмотрите все варианты</h2>
+      <div class="offer__solutions">
+        <div class="offer__solution">
+          <div class="offer__tag">Вариант 1</div>
+          <h3>
+            <span>Заплатить несколько</span>
+            <span>миллионов рублей</span>
+            <span>раскрученной студии</span>
+          </h3>
+          <ul>
+            <li>Да, вы с большой вероятностью получите качественный продукт с полным функционалом</li>
+            <li>Необязательно он будет с «ВАУ» — эффектом</li>
+            <li>И не факт, что вам предоставят «индивидуальный подход» к вашему проекту</li>
+          </ul>
+          <div class="offer__question">
+            Готовы ли вы вложить эти средства именно в сайт/сервис?
+            <svg><use href="/sprite.svg#question-comma" /></svg>
+          </div>
+        </div>
+        <div class="offer__solution">
+          <div class="offer__tag">Вариант 2</div>
+          <h3>Нанять фрилансеров по отдельности</h3>
+          <ul><li>Классный вариант сэкономить. Но это дополнительная головная боль</li></ul>
+          <div class="offer__clouds">
+            <div>А что с сайтом?</div>
+            <div>А как внедрить сервис?</div>
+            <div>А как грамотно наполнить сайт?</div>
+          </div>
+          <div class="offer__question">
+            Готовы ли вы вложить эти средства именно в сайт/сервис?
+            <svg><use href="/sprite.svg#question-comma" /></svg>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="offer__solution --best">
+      <div class="offer__wrapper --best">
+        <div>
+          <div class="offer__tag">Вариант 3</div>
+          <h2>Мы работаем под ключ комплексно</h2>
+          <Button class="offer__button --white" icon="lightning">Получить решение</Button>
+        </div>
+        <div>
+          <div class="offer__point-wrapper">
+            <div class="offer__point">
+              <svg><use href="/sprite.svg#bracket" /></svg>
+              <div>01</div>
+              <svg><use href="/sprite.svg#cursor" /></svg>
+              <svg><use href="/sprite.svg#bracket" /></svg>
+            </div>
+            <div class="offer__point-content">Погрузимся в ваши бизнес-процессы, спроектируем и создадим ваш проект</div>
+          </div>
+          <div class="offer__point-wrapper">
+            <div class="offer__point">
+              <svg><use href="/sprite.svg#bracket" /></svg>
+              <div>02</div>
+              <svg><use href="/sprite.svg#cursor" /></svg>
+              <svg><use href="/sprite.svg#bracket" /></svg>
+            </div>
+            <div class="offer__point-content">Разработаем, внедрим и обучим персонал использовать новую систему</div>
+          </div>
+          <div class="offer__point-wrapper">
+            <div class="offer__point">
+              <svg><use href="/sprite.svg#bracket" /></svg>
+              <div>03</div>
+              <svg><use href="/sprite.svg#human" /></svg>
+              <svg><use href="/sprite.svg#bracket" /></svg>
+            </div>
+            <div class="offer__point-content">
+              <div>У вас будет один подрядчик для всех вопросов и вам не придется искать</div>
+              <svg><use href="/sprite.svg#branches"/></svg>
+              <div>
+                <div>Копирайтеров для наполнения </div>
+                <div>Дизайнеров и программистов</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import Hero from '~/components/sections/Hero.vue'
 import DevTypes from '~/components/sections/DevTypes.vue'
+import Offer from '~/components/sections/Offer.vue'
 </script>
 
 <template>
   <Hero />
   <DevTypes />
+  <Offer />
   <div style="height: 200px; background: black; text-align: center;"></div>
 </template>
