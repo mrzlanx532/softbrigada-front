a16ae6b705040369d9734ecf60989cb5641e052e
Denis Danilov <d.danilov@duotek.ru>
2025-04-16 19:07:22 +0300

    Temp

---
+++ ecosystem.config.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps: [
+    {
+      name: 'SOFT_FRONT:PROD',
+      script: './.output/server/index.mjs',
+      port: 4000,
+      watch: false,
+      ignore_watch: ['./assets'],
+      log_date_format: 'YYYY-MM-DD HH:mm:ss',
+      error_file: './logs/error.log',
+      out_file: './logs/out.log',
+      max_memory_restart: '750M',
+    },
+  ],
+};
