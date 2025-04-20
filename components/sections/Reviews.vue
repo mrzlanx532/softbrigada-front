ece2beb2ea3e81512ea1a4951b42a8e845e207e1
Denis Danilov <d.danilov@duotek.ru>
2025-04-20 15:55:54 +0300

    Temp

--- components/sections/Reviews.vue
+++ components/sections/Reviews.vue
@@ -4,13 +4,14 @@ import { type Ref } from 'vue'
 const reviewRef = useTemplateRef('reviewRef')
 const reviewsRef = useTemplateRef('reviewsRef')
 
-
 const selectedReview: Ref<number | undefined> = ref(undefined)
-const page = ref(1)
-
-let everyReviewWidth = 0
-let everyPageWidth = 0
-let totalWidth = 0
+const sliderOptions = ref({
+  perPage: 3,
+  rewind : true,
+  type: 'loop',
+  arrows: false,
+  gap: '16px'
+})
 
 const reviews = ref([
   {
@@ -53,55 +54,36 @@ const onClickReview = (index: number) => {
   selectedReview.value = index
 }
 
-const onClickPage = (_page) => {
-  page.value = _page
-
-  const x = totalWidth * (_page - 1) * -1
-  reviewsRef.value.style.transform = 'translateX(' + x + 'px)'
+const onSplideMounted = () => {
+  /*reviewRef.value.forEach(el => {
+    el.children[0].style.width = window.getComputedStyle(el.offsetParent).width
+  })*/
 }
-
-onMounted(() => {
-  totalWidth = reviewsRef.value.getBoundingClientRect().width
-  everyReviewWidth = (totalWidth / 3) - (32 / 3)
-  everyPageWidth = everyReviewWidth * 3
-
-  reviewsRef.value.style.width = ((everyReviewWidth * reviews.value.length) + (16 * (reviews.value.length - 1))) + 'px'
-
-  reviewRef.value.forEach(el => {
-    el.style.width = everyReviewWidth + 'px'
-    el.children[0].style.width = el.offsetWidth + 'px'
-  })
-
-  document.querySelector('#reviews').style.opacity = 1
-})
 </script>
 
 <template>
   <section id="reviews" class="reviews">
     <h2>Отзывы</h2>
     <div class="reviews__reviews" ref="reviewsRef">
-      <div
-          v-for="(review, index) in reviews"
-          @click="onClickReview(index)"
-          class="reviews__review"
-          ref="reviewRef"
-          :class="{'--open': selectedReview === index}"
-      >
-        <div class="reviews__review-filter" />
-        <div class="reviews__review-head">
-          <img :src="'/images/reviews/' + (index + 1) + '.png'" alt="review-1">
-          <div>{{ review.title }}</div>
-        </div>
-        <div class="reviews__review-description">{{ review.description }}</div>
-      </div>
-    </div>
-    <div class="reviews__paginator">
-      <div
-          v-for="(_, index) in (Math.ceil(reviews.length / 3))"
-          class="reviews__paginator-pages"
-          :class="{'--active': page === (index + 1)}"
-          @click="onClickPage(index + 1)"
-      />
+      <ClientOnly>
+        <splide @splide:mounted="onSplideMounted" class="reviews__reviews-splide" :options="sliderOptions">
+          <splide-slide v-for="(review, index) in reviews" :key="index">
+            <div
+                @click="onClickReview(index)"
+                class="reviews__review"
+                ref="reviewRef"
+                :class="{'--open': selectedReview === index}"
+            >
+              <div class="reviews__review-filter" />
+              <div class="reviews__review-head">
+                <img :src="'/images/reviews/' + (index + 1) + '.png'" alt="review-1">
+                <div>{{ review.title }}</div>
+              </div>
+              <div class="reviews__review-description">{{ review.description }}</div>
+            </div>
+          </splide-slide>
+        </splide>
+      </ClientOnly>
     </div>
   </section>
 </template>
---
+++ plugins/splide.client.ts
@@ -0,0 +1,6 @@
+import VueSplide from '@splidejs/vue-splide'
+import '@splidejs/splide/dist/css/themes/splide-default.min.css';
+
+export default defineNuxtPlugin(nuxtApp => {
+    nuxtApp.vueApp.use(VueSplide)
+})
