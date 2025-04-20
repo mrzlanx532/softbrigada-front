10e115ec4c3dcec21ad4b424a3c8f184063ed04e
Denis Danilov <d.danilov@duotek.ru>
2025-04-20 18:20:32 +0300

    Temp

--- components/sections/SoftBrigada.vue
+++ components/sections/SoftBrigada.vue
@@ -1,9 +1,34 @@
 <script setup lang="ts">
+const softbrigadaSectionRef = useTemplateRef('softbrigadaSectionRef')
+const labelRef = useTemplateRef('labelRef')
+const spotlightRef = useTemplateRef('spotlightRef')
 
+const updateBackground = (x: number, y: number) => {
+  spotlightRef.value.style.background = `radial-gradient(circle at ${x}px ${y}px, #00000000 10px, #000000 350px)`;
+}
+
+const mouseMoveListener = (event) => {
+  setTimeout(() => {
+    updateBackground(event.clientX, event.clientY - softbrigadaSectionRef.value.getBoundingClientRect().top - 193);
+  }, 90);
+}
+
+const onMouseEnter = () => {
+  document.body.addEventListener('mousemove', mouseMoveListener)
+}
+
+const onMouseLeave = () => {
+  document.body.removeEventListener('mousemove', mouseMoveListener)
+}
+
+onMounted(() => {
+  document.querySelector('#softbrigada').style.visibility = 'visible'
+})
 </script>
 
 <template>
-  <section id="softbrigada" class="softbrigaga">
-    <h1>SoftBrigada</h1>
+  <section ref="softbrigadaSectionRef" id="softbrigada" class="softbrigaga" @mouseenter="onMouseEnter" @mouseleave="onMouseLeave">
+    <h1 ref="labelRef">SoftBrigada</h1>
+    <div ref="spotlightRef" class="softbrigaga__spotlight" />
   </section>
 </template>
