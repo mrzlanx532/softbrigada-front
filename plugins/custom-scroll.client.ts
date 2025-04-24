1b1ef49fa7988b0c073e7acbef30dd7dfeea966e
Denis Danilov <d.danilov@duotek.ru>
2025-04-24 15:25:48 +0300

    Temp

--- plugins/custom-scroll.client.ts
+++ plugins/custom-scroll.client.ts
@@ -1,9 +1,10 @@
 import { DocumentScroll } from '@mrzlanx532/nuxt-3-custom-scroll-plugin'
 
-import '@mrzlanx532/nuxt-3-custom-scroll-plugin/dist/css/common.css'
-import '@mrzlanx532/nuxt-3-custom-scroll-plugin/dist/css/document-scroll.no-ssr.css'
+// import '@mrzlanx532/nuxt-3-custom-scroll-plugin/dist/css/common.css'
+// import '@mrzlanx532/nuxt-3-custom-scroll-plugin/dist/css/document-scroll.no-ssr.css'
 
 export default defineNuxtPlugin((nuxtApp) => {
+    /*
     onNuxtReady(async () => {
         window.documentScroll = new DocumentScroll
 
@@ -15,4 +16,5 @@ export default defineNuxtPlugin((nuxtApp) => {
             console.warn('Не удалось установить ResizeObserver для document')
         }
     })
+    */
 })
