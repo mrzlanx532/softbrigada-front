fd82f600894a27bbed6e97738c0b303f8090f286
Denis Danilov <d.danilov@duotek.ru>
2025-05-02 20:20:09 +0300

    Temp

--- components/Menu.vue
+++ components/Menu.vue
@@ -15,26 +15,37 @@ const openBrief = () => {
 </script>
 
 <template>
-  <div class="menu">
-    <div class="menu__container">
-      <div class="menu__logo">
+  <div class="menu-desktop">
+    <div class="menu-desktop__container">
+      <div class="menu-desktop__logo">
         <img height="40" src="/images/logo/43.svg" alt="logo">
       </div>
-      <div class="menu__menu">
+      <div class="menu-desktop__menu">
         <a @click="onClickLink('dev-types')">что мы делаем</a>
         <a @click="onClickLink('cases')">наши кейсы <span>+3</span></a>
       </div>
     </div>
-    <div class="menu__container --right">
-      <div class="menu__contacts">
-        <div class="menu__contacts-social">
+    <div class="menu-desktop__container --right">
+      <div class="menu-desktop__contacts">
+        <div class="menu-desktop__contacts-social">
           <svg><use href="/sprite.svg#telegram" /></svg>
           <svg><use href="/sprite.svg#whatsapp" /></svg>
         </div>
         <a>info@gmail.com</a>
         <a>+7 (910) 292-03-10</a>
       </div>
-      <Button class="menu__button" @click="openBrief">Заполнить бриф</Button>
+      <Button class="menu-desktop__button" @click="openBrief">Заполнить бриф</Button>
+    </div>
+  </div>
+  <div class="menu-mobile">
+    <div class="menu-mobile__logo">
+      <img height="35" src="/images/logo/43.svg" alt="logo">
+    </div>
+    <div>
+      <Button class="menu-mobile__button" @click="openBrief">Заполнить бриф</Button>
+      <Button class="menu-mobile__button">
+        <svg><use href="/sprite.svg#burger" /></svg>
+      </Button>
     </div>
   </div>
 </template>
