ad689fc0e9f8fc96c815b127c97e24e0ecc3019a
Denis Danilov <d.danilov@duotek.ru>
2025-05-04 13:03:25 +0300

    Temp

--- composables/useAnchor.ts
+++ composables/useAnchor.ts
@@ -1,10 +1,6 @@
 export default () => {
     const goToAnchor = (id: string) => {
-        const a = document.createElement('a')
-        a.href = `#${id}`
-        a.click()
-
-        window.history.pushState('', document.title, window.location.pathname)
+        document.getElementById(id)?.scrollIntoView({ behavior: 'smooth' })
     }
 
     return {
