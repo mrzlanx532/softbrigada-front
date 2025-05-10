6009cde9d5ac71d4d9a191e18024f16284eda6c4
Denis Danilov <d.danilov@duotek.ru>
2025-05-10 16:51:04 +0300

    Temp

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,9 +11,6 @@ export default defineNuxtConfig({
       meta: [{
         name: 'viewport',
         content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'
-      }, {
-        name: 'apple-mobile-web-app-capable',
-        content: 'yes'
       }]
     }
   },
