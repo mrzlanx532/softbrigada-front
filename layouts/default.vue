244e197c771a0bc4c36e1998687b25c0d1f8fe43
Denis Danilov <d.danilov@duotek.ru>
2025-09-22 19:05:02 +0300

    Added vue-final-modal

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import Brief from '~/components/Brief.vue'
+import { ModalsContainer } from 'vue-final-modal'
 </script>
 
 <template>
+  <ModalsContainer />
   <Brief />
   <slot />
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,7 +11,7 @@ export default defineNuxtConfig({
   },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: false },
-  modules: ['v-gsap-nuxt', 'nuxt-svgo', '@nuxtjs/device'],
+  modules: ['v-gsap-nuxt', 'nuxt-svgo', '@nuxtjs/device', '@vue-final-modal/nuxt'],
   css: ['~/assets/scss/app.scss'],
   app: {
     head: {
