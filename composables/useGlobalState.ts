f72436e0f4bd0e258947f3becb46ddf4fc1aa5a4
Denis Danilov <d.danilov@duotek.ru>
2025-09-22 19:32:37 +0300

    temp

--- composables/useGlobalState.ts
+++ composables/useGlobalState.ts
@@ -3,6 +3,26 @@ export default () => {
     const burgerMenuIsOpen = useState('burgerMenuIsOpen', () => false)
     const widgetIsVisible = useState('widgetIsVisible', () => true)
 
+    const scrollBarWidthWasRead = useState('scrollBarWidthWasRead', () => false)
+    const scrollBarWidth = useState('scrollBarWidth', () => 0)
+
+    watch(briefIsOpen, (val) => {
+
+        if (!(scrollBarWidthWasRead.value)) {
+            scrollBarWidth.value = window.innerWidth - document.documentElement.clientWidth
+            scrollBarWidthWasRead.value = true
+        }
+
+        if (val) {
+            document.body.style.overflow = 'hidden'
+            document.body.style.paddingRight = scrollBarWidth.value + 'px'
+            return
+        }
+
+        document.body.style.overflow = ''
+        document.body.style.paddingRight = ''
+    })
+
     return {
         briefIsOpen,
         burgerMenuIsOpen,
